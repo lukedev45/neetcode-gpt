870dedeb448e51057240da006c96626b335fbf4f
Luke Devlin <36817334+lukedev45@users.noreply.github.com>
2026-05-15 10:52:31 +0100

    Update Softmax function (Math Foundations 3/6)

--- foundations/softmax.py
+++ foundations/softmax.py
@@ -3,7 +3,6 @@ from numpy.typing import NDArray
 
 
 class Solution:
-
     def softmax(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
         # z is a 1D NumPy array of logits
         # Hint: subtract max(z) for numerical stability before computing exp
